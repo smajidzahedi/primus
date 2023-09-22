3fa7c6897c72a5c71a905fbad23228887eeb50c9
Huangmao1208 <quietwu94@gmail.com>
2023-09-21 23:26:44 -0400

    update code, finding that print after-trained threshold value does seems mean nothing

--- multiprocessing_MARL.py
+++ multiprocessing_MARL.py
@@ -660,7 +660,7 @@ class Worker:
             self.w2c_queue.put((server_ids, actions))
 
 
-def main(config_file_name, train):
+def main(config_file_name):
     start_time = time.time()
     with open(config_file_name, 'r') as f:
         config = json.load(f)
@@ -680,113 +680,90 @@ def main(config_file_name, train):
     path = f"{folder_name}/{num_servers}_server/{app_type}"
     if not os.path.exists(path):
         os.makedirs(path)
-    if train == True:
-        w2c_queues = [Queue() for _ in range(num_workers)]
-        c2w_queues = [Queue() for _ in range(num_workers)]
-        servers = []
-        worker_processors = []
-        servers_per_worker = num_servers // num_workers
-        coordinator = Coordinator(coordinator_config, w2c_queues, c2w_queues, path, num_workers, num_servers, game_type, app_type, policy_type, threshold, alpha, epsilon_bar)
-        for i in range(num_servers):
-            app = None
-            if "markov_app" == app_type:
-                transition_matrix = config["transition_matrix"]
-                app = MarkovApp(app_states, transition_matrix, utilities, np.random.choice(app_states))
-            elif "uniform_app" == app_type:
-                app = UniformApp(app_states, utilities)
-            elif "normal_app" == app_type:
-                app = NormalApp(app_states, utilities)
-            elif "queue_app" == app_type:
-                arrival_tps = config["arrival_tps"]
-                sprinting_tps = config["sprinting_tps"]
-                nominal_tps = config["nominal_tps"]
-                max_queue_length = config["max_queue_length"]
-                app = QueueApp(utilities, arrival_tps, sprinting_tps, nominal_tps, max_queue_length)
-            if "ac_policy" == policy_type:
-                lr_actor = config["lr_actor"]
-                lr_critic = config["lr_critic"]
-                l1_in_actor = config["l1_in_actor"]
-                l1_in_critic = config["l1_in_critic"]
-                l1_out_l2_in_actor = config["l1_out_l2_in_actor"]
-                l1_out_l2_in_critic = config["l1_out_l2_in_critic"]
-                policy = ACPolicy(l1_in_actor, l1_in_critic, l1_out_l2_in_actor, l1_out_l2_in_critic, app_type)
-                # Create an Adam optimizer for the actor
-                if app_type == "queue_app":
-                    actor_params = [policy.actor_layer1.weight, policy.actor_layer1.bias,
-                                    policy.actor_layer2_mean.weight, policy.actor_layer2_mean.bias,
-                                    policy.actor_layer2_std.weight, policy.actor_layer2_std.bias]
-                else:
-                    actor_params = [policy.actor_layer1.weight, policy.actor_layer1.bias,
-                                    policy.actor_layer2.weight, policy.actor_layer2.bias]
-
-                actor_optimizer = optim.Adam(actor_params, lr=lr_actor)
-
-                # Create an Adam optimizer for the critic
-                critic_params = [policy.critic_layer1.weight, policy.critic_layer1.bias,
-                                policy.critic_layer2.weight, policy.critic_layer2.bias]
-
-                critic_optimizer = optim.Adam(critic_params, lr=lr_critic)
-                optimizer = [actor_optimizer, critic_optimizer]
-                server = AC_server(i, policy, optimizer, app, path, servers_config, num_servers, servers_per_worker, game_type)
-            elif "thr_policy" == policy_type:
-                policy = ThrPolicy(threshold)
-                server = Thr_server(i, policy, app, path, servers_config, num_servers, servers_per_worker, game_type)
-            elif "q_learning" == policy_type:
-                lr_q = config["lr_q"]
-                l1_in_q = config["l1_in_q"]
-                l1_out_l2_in_q = config["l1_out_l2_in_q"]
-                dqn = QNetwork(l1_in_q, l1_out_l2_in_q)
-                target_dqn = QNetwork(l1_in_q, l1_out_l2_in_q)
-                optimizer = optim.Adam(dqn.parameters(), lr_q)
-                server = Q_server(i, dqn, target_dqn, optimizer, app, path, servers_config, num_servers, servers_per_worker, game_type)
-            servers.append(server)
-        
-        split_servers = np.array_split(servers, num_workers)
-        for i, s in enumerate(split_servers):
-            worker = Worker(s, w2c_queues[i], c2w_queues[i])
-            worker_processor = Process(target=worker.run_worker)
-            worker_processors.append(worker_processor)
-            worker_processor.start()
-
-        coordinator_processor = Process(target=coordinator.run_coordinator)
-        coordinator_processor.start()
-
-        for worker_processor in worker_processors:
-            worker_processor.join()
-        
-        coordinator_processor.join()
-
-        end_time = time.time()
-        total_time = end_time - start_time
-        print(f"Total running time: {total_time} seconds")
-        if policy_type == "ac_policy":
-            if coordinator_config["noise"] == 0:
-                torch.save(policy.state_dict(), f"/Users/jingyiwu/Desktop/MARL/model/{app_type}_no_noise_dict.pth")
+    w2c_queues = [Queue() for _ in range(num_workers)]
+    c2w_queues = [Queue() for _ in range(num_workers)]
+    servers = []
+    worker_processors = []
+    servers_per_worker = num_servers // num_workers
+    coordinator = Coordinator(coordinator_config, w2c_queues, c2w_queues, path, num_workers, num_servers, game_type, app_type, policy_type, threshold, alpha, epsilon_bar)
+    policies = []
+    for i in range(num_servers):
+        app = None
+        if "markov_app" == app_type:
+            transition_matrix = config["transition_matrix"]
+            app = MarkovApp(app_states, transition_matrix, utilities, np.random.choice(app_states))
+        elif "uniform_app" == app_type:
+            app = UniformApp(app_states, utilities)
+        elif "normal_app" == app_type:
+            app = NormalApp(app_states, utilities)
+        elif "queue_app" == app_type:
+            arrival_tps = config["arrival_tps"]
+            sprinting_tps = config["sprinting_tps"]
+            nominal_tps = config["nominal_tps"]
+            max_queue_length = config["max_queue_length"]
+            app = QueueApp(utilities, arrival_tps, sprinting_tps, nominal_tps, max_queue_length)
+        if "ac_policy" == policy_type:
+            lr_actor = config["lr_actor"]
+            lr_critic = config["lr_critic"]
+            l1_in_actor = config["l1_in_actor"]
+            l1_in_critic = config["l1_in_critic"]
+            l1_out_l2_in_actor = config["l1_out_l2_in_actor"]
+            l1_out_l2_in_critic = config["l1_out_l2_in_critic"]
+            policy = ACPolicy(l1_in_actor, l1_in_critic, l1_out_l2_in_actor, l1_out_l2_in_critic, app_type)
+            # Create an Adam optimizer for the actor
+            if app_type == "queue_app":
+                actor_params = [policy.actor_layer1.weight, policy.actor_layer1.bias,
+                                policy.actor_layer2_mean.weight, policy.actor_layer2_mean.bias,
+                                policy.actor_layer2_std.weight, policy.actor_layer2_std.bias]
             else:
-                torch.save(policy.state_dict(), f"/Users/jingyiwu/Desktop/MARL/model/{app_type}_noise_dict.pth")
-    else:
-        if coordinator_config["noise"] == 0:
-            policy_state_dict = torch.load(f"/Users/jingyiwu/Desktop/MARL/model/{app_type}_no_noise_dict.pth")
-        else:
-            policy_state_dict = torch.load(f"/Users/jingyiwu/Desktop/MARL/model/{app_type}_noise_dict.pth")
-        lr_actor = config["lr_actor"]
-        lr_critic = config["lr_critic"]
-        l1_in_actor = config["l1_in_actor"]
-        l1_in_critic = config["l1_in_critic"]
-        l1_out_l2_in_actor = config["l1_out_l2_in_actor"]
-        l1_out_l2_in_critic = config["l1_out_l2_in_critic"]
-        policy = ACPolicy(l1_in_actor, l1_in_critic, l1_out_l2_in_actor, l1_out_l2_in_critic, app_type)
-        policy.load_state_dict(policy_state_dict)
-        policy.eval()
-        input_actor = torch.tensor([0.25])
-        input_critic = torch.cat((torch.tensor([0]), torch.tensor([0]), torch.tensor([0]), torch.tensor([0.1])))
-        with torch.no_grad():  # Disable gradient calculation to save memory and speed up inference
-            threshold, _ = policy([input_actor, input_critic])
-        print(f"{app_type}: Fractional number of sprinter: {f}, returned threshold: {threshold}")
+                actor_params = [policy.actor_layer1.weight, policy.actor_layer1.bias,
+                                policy.actor_layer2.weight, policy.actor_layer2.bias]
 
+            actor_optimizer = optim.Adam(actor_params, lr=lr_actor)
+
+            # Create an Adam optimizer for the critic
+            critic_params = [policy.critic_layer1.weight, policy.critic_layer1.bias,
+                            policy.critic_layer2.weight, policy.critic_layer2.bias]
+
+            critic_optimizer = optim.Adam(critic_params, lr=lr_critic)
+            optimizer = [actor_optimizer, critic_optimizer]
+            server = AC_server(i, policy, optimizer, app, path, servers_config, num_servers, servers_per_worker, game_type)
+        elif "thr_policy" == policy_type:
+            policy = ThrPolicy(threshold)
+            server = Thr_server(i, policy, app, path, servers_config, num_servers, servers_per_worker, game_type)
+        elif "q_learning" == policy_type:
+            lr_q = config["lr_q"]
+            l1_in_q = config["l1_in_q"]
+            l1_out_l2_in_q = config["l1_out_l2_in_q"]
+            dqn = QNetwork(l1_in_q, l1_out_l2_in_q)
+            target_dqn = QNetwork(l1_in_q, l1_out_l2_in_q)
+            optimizer = optim.Adam(dqn.parameters(), lr_q)
+            server = Q_server(i, dqn, target_dqn, optimizer, app, path, servers_config, num_servers, servers_per_worker, game_type)
+        servers.append(server)
+        policies.append(policy)
+    
+    split_servers = np.array_split(servers, num_workers)
+    for i, s in enumerate(split_servers):
+        worker = Worker(s, w2c_queues[i], c2w_queues[i])
+        worker_processor = Process(target=worker.run_worker)
+        worker_processors.append(worker_processor)
+        worker_processor.start()
+
+    coordinator_processor = Process(target=coordinator.run_coordinator)
+    coordinator_processor.start()
+
+    for worker_processor in worker_processors:
+        worker_processor.join()
+    
+    coordinator_processor.join()
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"Total running time: {total_time} seconds")
+    
 
 if __name__ == "__main__":
-    config_file = "/Users/jingyiwu/Desktop/MARL/config.json"
-    main(config_file, train = False)
+    config_file = "/Users/jingyiwu/Desktop/MARL/markov_app_noise_config.json"
+    main(config_file)
 
     
